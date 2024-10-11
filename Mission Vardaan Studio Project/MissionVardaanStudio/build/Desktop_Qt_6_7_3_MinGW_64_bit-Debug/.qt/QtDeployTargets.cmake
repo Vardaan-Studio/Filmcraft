3e3c8d136ba942e4648ab6b69e35abd00063eb7f
rohit <rohitdmore2255@gmail.com>
2024-10-11 15:40:46 +0530

    Files removed

--- Mission Vardaan Studio Project/MissionVardaanStudio/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MissionVardaanStudio_FILE E:/Mission Vardaan Studio Project/MissionVardaanStudio/build/Desktop_Qt_6_7_3_MinGW_64_bit-Debug/MissionVardaanStudio.exe)
-set(__QT_DEPLOY_TARGET_MissionVardaanStudio_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MissionVardaanStudio_RUNTIME_DLLS C:/Qt/6.7.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.3/mingw_64/bin/Qt6Core.dll)
